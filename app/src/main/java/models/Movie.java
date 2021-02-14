e09fa5e4308f65a4e7e80d5723f5bccdaef747f2
Brian Tan <briankptan@gmail.com>
2021-02-14 01:35:54 -0800

    myFlix Part 2

--- app/src/main/java/models/Movie.java
+++ app/src/main/java/models/Movie.java
@@ -3,18 +3,27 @@ package models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Parcel
 public class Movie {
     String posterPath, title, overview, backdropPath;
+    double rating;
+    int movieId;
+
+    //Empty constructor needed for Parceler library
+    public Movie(){}
 
     public Movie(JSONObject jsonObject) throws JSONException {
         backdropPath = jsonObject.getString("backdrop_path");
         posterPath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
+        rating = jsonObject.getDouble("vote_average");
+        movieId = jsonObject.getInt("id");
     }
 
     public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
@@ -39,5 +48,11 @@ public class Movie {
 
     public String getOverview() {
         return overview;
+    }
+
+    public double getRating(){ return rating;}
+
+    public int getMovieId() {
+        return movieId;
     }
 }
